2515839a7ef083765dea821ce95440c3e3dec054
Luis Felipe Schenone <schenonef@gmail.com>
2014-11-21 10:28:28 -0300

    Updating all
    Change-Id: I2043fcf0ae022591b4d72759c5bedfe57debc1ea
    (cherry picked from commit bc0077d9a7777458cf70be97f72fce85038a4adf)

---
+++ MsLinks.body.php
@@ -0,0 +1,73 @@
+<?php
+
+class MsLinks {
+
+	static function start() {
+		global $wgOut, $wgJsMimeType, $wgScriptPath;
+		$wgOut->addModules( 'ext.MsLinks' );
+		$path = $wgScriptPath . '/extensions/MsLinks';
+		$wgOut->addScript( "<script type=\"$wgJsMimeType\">var msl_icon_path = \"$path\";</script>" );
+		return true;
+	}
+
+	static function setHook( &$parser ) {
+		$parser->setFunctionHook( 'mslink', 'MsLinks::render' );
+		return true;
+	}
+
+	static function getMagicWord( &$magicWords, $langCode ) {
+		$magicWords['mslink'] = array( 0, 'l' );
+		return true;
+	}
+
+	static function render( $parser, $type = 'no', $url = '', $description = '', $align = '' ) {
+		global $wgOut, $wgScriptPath, $wgMSL_FileTypes;
+
+		if ( $type !== 'dlink' and $type !== 'vlink' ) {
+			$align = $description;
+			$description = $url;
+			$url = $type;
+		}
+
+		try {
+			$title = Title::newFromText( $url, NS_IMAGE );
+			$file = function_exists( 'wfFindFile' ) ? wfFindFile( $title ) : new Image( $title );
+			$base = ( is_object( $file ) && $file->exists() ) ? ':Image' : 'Media';
+		} catch( Exception $exception ) {
+			$base = 'Media';
+		} 
+
+		$extension = strtolower( substr( strrchr( $url, '.' ), 1 ) );
+		if ( !$description ) {
+			if ( $extension ) {
+				$description = substr( $url, 0, ( strlen( $url ) - ( strlen( $extension ) + 1 ) ) );
+			} else {
+				$description = $url;
+			}
+		}
+
+		//Defaults
+		$wikitext = "[[$base:$url|$description]]";
+		$image = "<img src=\"$wgScriptPath/extensions/MsLinks/images/" . $wgMSL_FileTypes['no'] . "\">";
+
+		foreach ( $wgMSL_FileTypes as $key => $value ) { 
+			if ( $key === $extension ) {
+				$image = "<img title=\"$extension\" src=\"$wgScriptPath/extensions/MsLinks/images/$value\">"; 
+			}
+		}
+		$image = $parser->insertStripItem( $image, $parser->mStripState );
+
+		if ( $type !== 'vlink' and $type !== 'dlink' ) {
+			$base = 'Media';
+		}
+
+		$image = "[[$base:$url|$image]]";
+
+		if ( $align === 'right' ) { 
+			$wikitext = $wikitext . ' ' . $image;
+		} else {
+			$wikitext = $image . ' ' . $wikitext;
+		}
+		return $wikitext;
+	}
+}
