bc0077d9a7777458cf70be97f72fce85038a4adf
Luis Felipe Schenone <schenonef@gmail.com>
2014-11-21 10:28:28 -0300

    Updating all
    Change-Id: I2043fcf0ae022591b4d72759c5bedfe57debc1ea

---
+++ MsLinks.php
@@ -0,0 +1,71 @@
+<?php
+
+$wgExtensionCredits['parserhook'][] = array(
+	'name' => 'MsLinks',
+	'url' => 'https://www.mediawiki.org/wiki/Extension:MsLinks',
+	'version' => '3.0',
+	'descriptionmsg' => 'msl-desc',
+	'license-name' => 'GPLv2+',
+	'author' => array( '[mailto:wiki@ratin.de Martin Schwindl]', '[mailto:wiki@keyler-consult.de Martin Keyler]', '[https://www.mediawiki.org/wiki/User:Luis_Felipe_Schenone Luis Felipe Schenone]' ),
+);
+
+$wgResourceModules['ext.MsLinks'] = array(
+	'scripts' => 'MsLinks.js',
+	'messages' => array(
+		'msl-tooltip',
+		'msl-example-filename',
+	),
+	'localBasePath' => __DIR__,
+	'remoteExtPath' => 'MsLinks',
+);
+
+$wgAutoloadClasses['MsLinks'] = __DIR__ . '/MsLinks.body.php';
+
+$wgExtensionMessagesFiles['MsLinks'] = __DIR__ . '/MsLinks.i18n.php';
+$wgMessagesDirs['MsLinks'] = __DIR__ . '/i18n';
+
+$wgHooks['ParserFirstCallInit'][] = 'MsLinks::setHook';
+$wgHooks['EditPage::showEditForm:initial'][] = 'MsLinks::start';
+$wgHooks['LanguageGetMagic'][] = 'MsLinks::getMagicWord';
+
+// Default configuration
+$wgMSL_FileTypes = array(
+	'no' => 'no_icon.png',
+	'jpg' => 'image_icon.png',
+	'gif' => 'image_icon.png',
+	'bmp' => 'image_icon.png',
+	'png' => 'image_icon.png',
+	'tiff' => 'image_icon.png',
+	'tif' => 'image_icon.png',
+	'ai' => 'image_ai_icon.png',
+	'psd' => 'image_ps_icon.png',
+	'pdf' => 'pdf_icon.png',
+	'pps' => 'pps_icon.png',
+	'ppt' => 'pps_icon.png',
+	'pptx' => 'pps_icon.png',
+	'xls' => 'xls_icon.png',
+	'xlsx' => 'xls_icon.png',
+	'doc' => 'doc_icon.png',
+	'docx' => 'doc_icon.png',
+	'dot' => 'doc_icon.png',
+	'dotx' => 'doc_icon.png',
+	'rtf' => 'doc_icon.png',
+	'txt' => 'txt_icon.png',
+	'html' => 'code_icon.png',
+	'php' => 'code_icon.png',
+	'exe' => 'exe_icon.gif',
+	'asc' => 'txt_icon.png',
+	'dwg' => 'dwg_icon.gif',
+	'zip' => 'zip_icon.png',
+	'mov' => 'movie_icon.png',
+	'mpeg' => 'movie_icon.png',
+	'mpg' => 'movie_icon.png',
+	'wmv' => 'movie_icon.png',
+	'avi' => 'movie_icon.png',
+	'mp4' => 'movie_icon.png',
+	'flv' => 'movie_flash_icon.png',
+	'wma' => 'music_icon.png',
+	'mp3' => 'music_icon.png',
+	'wav' => 'music_icon.png',
+	'mid' => 'music_icon.png',
+);
--- mslinks.js
+++ mslinks.js
@@ -1,70 +1,47 @@
-if(typeof(add_buttons) != 'undefined'){
-	add_buttons.push("msl");
-}
-		
-$(document).ready(function () { //jquery
-	
-	var img_msl = "extensions/MsLinks/images/Wiki-Editor-Buttons_Li.png";
-	var img_msl2 = path_button_msl +"/images/Wiki-Editor-Buttons_Li.png";
-	var nam_msl = "Direktlink";
-	var pre_msl = "{{#l:";
-	var peri_msl = "Filename.ext";
-	var post_msl = "\}\}";
-	
-	if ( $.inArray( mw.config.get( 'wgAction' ), ['edit', 'submit'] ) !== -1 ) {
-        mw.loader.using( 'user.options', function () {
-                if ( mw.user.options.get('usebetatoolbar') ) {
-                        mw.loader.using( 'ext.wikiEditor.toolbar', function () {
-                                
-                                if(typeof(addThisButton) == 'undefined'){
-                                addGroup_msl();
-								addButton_msl(nam_msl,pre_msl,peri_msl,post_msl,img_msl2);	
-								}
-                                return true;
-                        } );
-                }
-        } );
-        
-        mw.toolbar.addButton({
-	        "imageFile": img_msl, 
-	        "speedTip": nam_msl,
-	        "tagOpen": pre_msl, 
-	        "tagClose": post_msl,     
-	        "sampleText": peri_msl  
-		});
-	}	
-});
+var msl_icon = msl_icon_path + "/images/mslink_icon.png";
+var msl_pre = "{{#l:";
+var msl_peri = mw.msg( 'msl-example-filename' );
+var msl_post = "}}";
 
-function addGroup_msl(){
-	// To add a group to an existing toolbar section:
+if ( $.inArray( mw.config.get( 'wgAction' ), [ 'edit', 'submit' ] ) !== -1 ) {
+	mw.loader.using( 'user.options', function () {
+		if ( mw.user.options.get( 'usebetatoolbar' ) && mw.user.options.get( 'showtoolbar' ) ) {
+			mw.loader.using( 'ext.wikiEditor.toolbar', msl_addButton1 );
+		} else {
+			mw.loader.using( 'mediawiki.action.edit', msl_addButton2 );
+		}
+	});
+}
+
+function msl_addButton1() {
 	$( '#wpTextbox1' ).wikiEditor( 'addToToolbar', {
-    	'section': 'main',
-        'groups': {
-        	'MsLinks': {}
-    	}
-    });
+		'section': 'main',
+		'group': 'insert',
+		'tools': {
+			'MsLink': {
+				'label': mw.msg( 'msl-tooltip' ),
+				'type': 'button',
+				'icon': msl_icon,
+				'action': {
+					'type': 'encapsulate',
+					'options': {
+						'pre': msl_pre, 
+						'peri': msl_peri,
+						'post': msl_post,
+					}
+				}
+			}
+		}
+	});
 }
 
-function addButton_msl(nam,prex,perix,postx,img){
-
-	// To add a button to an existing toolbar group:
-	$('#wpTextbox1' ).wikiEditor( 'addToToolbar', {
-	'section': 'main',
-	'group': 'MsLinks',
-	'tools': {
-		 nam: {
-		    label: nam, // or use labelMsg for a localized label, see above
-		    type: 'button',
-		    icon: img,
-		    action: {
-		    	type: 'encapsulate',
-		        options: {
-		           pre: prex, 
-		           peri: perix,
-		           post: postx,
-		        }
-		    }
-		 }
-	}
+function msl_addButton2() {
+	mw.toolbar.addButton({
+		imageFile: msl_icon,
+		speedTip: mw.msg( 'msl-tooltip' ),
+		tagOpen: msl_pre,
+		tagClose: msl_post,
+		sampleText: msl_peri,
+		imageId: 'mslink'
 	});
 }
