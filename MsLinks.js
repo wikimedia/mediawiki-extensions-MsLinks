e168dccc28396a1e711213fd38ea39c86dc98f73
Sophivorus <schenonef@gmail.com>
2021-04-10 13:15:11 -0700

    Fix for latest MediaWiki
    Change-Id: I646164ee0b0266606ea30d5bd8e7728cf054e08f

--- MsLinks.js
+++ MsLinks.js
@@ -1,3 +1,4 @@
+var msl_tooltip = mw.msg( 'msl-tooltip' );
 var msl_icon = msl_icon_path + "/images/mslink_icon.png";
 var msl_pre = "{{#l:";
 var msl_peri = mw.msg( 'msl-example-filename' );
@@ -7,7 +8,7 @@ if ( $.inArray( mw.config.get( 'wgAction' ), [ 'edit', 'submit' ] ) !== -1 ) {
 	mw.loader.using( 'user.options', function () {
 		if ( mw.user.options.get( 'usebetatoolbar' ) ) {
 			$.when(
-				mw.loader.using( 'ext.wikiEditor.toolbar' ), $.ready
+				mw.loader.using( 'ext.wikiEditor' ), $.ready
 			).then( msl_addButton1 );
 		} else {
 			mw.loader.using( 'mediawiki.action.edit', msl_addButton2 );
@@ -17,24 +18,24 @@ if ( $.inArray( mw.config.get( 'wgAction' ), [ 'edit', 'submit' ] ) !== -1 ) {
 
 function msl_addButton1() {
 	$( '#wpTextbox1' ).wikiEditor( 'addToToolbar', {
-		'section': 'main',
-		'group': 'insert',
-		'tools': {
-			'MsLink': {
-				'label': mw.msg( 'msl-tooltip' ),
-				'type': 'button',
-				'icon': msl_icon,
-				'action': {
-					'type': 'encapsulate',
-					'options': {
-						'pre': msl_pre, 
-						'peri': msl_peri,
-						'post': msl_post,
+        section: 'main',
+        group: 'insert',
+        tools: {
+            MsLink: {
+                label: msl_tooltip,
+                type: 'button',
+                icon: msl_icon,
+                action: {
+                    type: 'encapsulate',
+					options: {
+						pre: msl_pre, 
+						peri: msl_peri,
+						post: msl_post
 					}
-				}
-			}
-		}
-	});
+                }
+            }
+        }
+    } );
 }
 
 function msl_addButton2() {
